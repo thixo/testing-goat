73bc2938e8f5790d84925b6422a42c0a695a8aa8
msanders <thixo@mac.com>
2016-04-25 20:56:24 +0100

    Add unittest asserts and implicit_wait()

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -40,4 +41,5 @@ class NewVisitorTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    unittest.main()
+
